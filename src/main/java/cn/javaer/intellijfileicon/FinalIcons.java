55ab4c4f3169cb074d76c129b43ba41e93463dfc
cn-src <public@javaer.cn>
2018-06-23 01:18:38 +0800

    update

--- src/main/java/cn/javaer/intellijfileicon/FinalIcons.java
+++ src/main/java/cn/javaer/intellijfileicon/FinalIcons.java
@@ -12,25 +12,25 @@ import java.util.Map;
  * @author zhangpeng
  */
 public class FinalIcons {
-    private static final Map<String, Icon> fileIconMapping;
+    private static final Map<String, Icon> ICONS;
 
     static {
-        fileIconMapping = new HashMap<>();
-        fileIconMapping.put(".csv", IconLoader.getIcon("/icons/file_type_csv.png"));
-        fileIconMapping.put(".cmd", IconLoader.getIcon("/icons/file_type_windows.png"));
-        fileIconMapping.put(".bat", IconLoader.getIcon("/icons/file_type_windows.png"));
-//        fileIconMapping.put(".sh", IconLoader.getIcon("/icons/file_type_shell.png"));
-        fileIconMapping.put(".md", IconLoader.getIcon("/icons/MarkdownPlugin.png"));
-        fileIconMapping.put(".props", IconLoader.getIcon("/icons/props.png"));
-        fileIconMapping.put("prop@2x", IconLoader.getIcon("/icons/props@2x.png"));
-        fileIconMapping.put(".travis.yml", IconLoader.getIcon("/icons/file_type_travis.png"));
-        fileIconMapping.put(".kt", IconLoader.getIcon("/icons/kotlin.png"));
+        ICONS = new HashMap<>();
+        ICONS.put(".csv", IconLoader.getIcon("/icons/file_type_csv.png"));
+        ICONS.put(".cmd", IconLoader.getIcon("/icons/file_type_windows.png"));
+        ICONS.put(".bat", IconLoader.getIcon("/icons/file_type_windows.png"));
+//        ICONS.put(".sh", IconLoader.getIcon("/icons/file_type_shell.png"));
+        ICONS.put(".md", IconLoader.getIcon("/icons/MarkdownPlugin.png"));
+        ICONS.put(".props", IconLoader.getIcon("/icons/props.png"));
+        ICONS.put("prop@2x", IconLoader.getIcon("/icons/props@2x.png"));
+        ICONS.put(".travis.yml", IconLoader.getIcon("/icons/file_type_travis.png"));
+        ICONS.put(".kt", IconLoader.getIcon("/icons/kotlin.png"));
     }
 
     private FinalIcons() {}
 
     public static Icon get(final String fileName) {
-        return fileIconMapping.entrySet().stream()
+        return ICONS.entrySet().stream()
                 .filter(entry -> fileName.endsWith(entry.getKey()))
                 .findFirst()
                 .map(Map.Entry::getValue)
