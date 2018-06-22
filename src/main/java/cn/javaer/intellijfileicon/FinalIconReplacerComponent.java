ec4b8967f5a5c9bc063b345f404660631efcb4e7
cn-src <public@javaer.cn>
2018-06-22 22:42:48 +0800

    更改 shell 图标

--- src/main/java/cn/javaer/intellijfileicon/FinalIconReplacerComponent.java
+++ src/main/java/cn/javaer/intellijfileicon/FinalIconReplacerComponent.java
@@ -31,5 +31,8 @@ public class FinalIconReplacerComponent implements ApplicationComponent {
         Utils.classForName("jodd.idea.props.Props")
                 .ifPresent(clz ->
                         Utils.setFinalStatic(clz, "ICON_32", FinalIcons.get("prop@2x")));
+        Utils.classForName("com.ansorgit.plugins.bash.util.BashIcons")
+                .ifPresent(clz ->
+                        Utils.setFinalStatic(clz, "BASH_FILE_ICON", FinalIcons.get("sh")));
     }
 }
--- src/main/java/cn/javaer/intellijfileicon/FinalIcons.java
+++ src/main/java/cn/javaer/intellijfileicon/FinalIcons.java
@@ -19,6 +19,7 @@ public class FinalIcons {
         fileIconMapping.put("csv", IconLoader.getIcon("/icons/file_type_csv.png"));
         fileIconMapping.put("cmd", IconLoader.getIcon("/icons/file_type_powershell.png"));
         fileIconMapping.put("bat", IconLoader.getIcon("/icons/file_type_powershell.png"));
+        fileIconMapping.put("sh", IconLoader.getIcon("/icons/file_type_shell.png"));
         fileIconMapping.put("md", IconLoader.getIcon("/icons/MarkdownPlugin.png"));
         fileIconMapping.put("prop", IconLoader.getIcon("/icons/prop.png"));
         fileIconMapping.put("prop@2x", IconLoader.getIcon("/icons/prop@2x.png"));
